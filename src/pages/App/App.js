b7b4265079fedbb39c48465c3a3717727c249771
David Sheinbein <davesheinbein@gmail.com>
2020-09-04 02:43:35 -0700

    init commit

---
+++ src/pages/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './/styles/App.css';
+
+
+class App extends Component {
+	/*--- State ---*/
+	/*--- Handle Methods ---*/
+	/*--- Lifecycle Methods ---*/
+	render() {
+		return (
+			<div className='App'>
+				<header className='App-header'>Chat Bot</header>
+			</div>
+		);
+	}
+}
+export default App;
